415522becef54b4626fad21493c2cd0620b4527d
鱼101 <9991171+fish-101@user.noreply.gitee.com>
2022-05-09 18:00:53 +0800

    yzh

--- react-app/src/component/data.js
+++ react-app/src/component/data.js
@@ -4,26 +4,30 @@ import React, {
 import {
     dataList
 } from '../utils/dataList';
+import moment from 'moment'
+const a = moment().daysInMonth()
 class Banner extends Component {
     state = {
-        list: dataList
+        list: dataList,
+        day: a
     }
     render() {
         console.log(this.state.list)
+        console.log(this.state.day);
         return (
             <div>
-                <div>
+                <div style={{ display: "flex", width: '100%' }}>
                     {
                         this.state.list.map((l, i) => (
-                            <div className="box" key={i} style={{ width: '200px', height: '50px', background: 'yellow', float: 'left', textAlign: 'center' }}>
+                            <div className="box" key={i} style={{ height: '50px', background: 'yellow', textAlign: 'center', flex: 1 }}>
                                 {l.value}
                             </div>
                         ))
                     }
                 </div>
                 <div>
-                    <div className="bigbox" style={{ width: '200px', height: "200px", background: "bule" }}>
-
+                    <div className="bigbox" style={{ width: '200px', height: "200px", background: "blue" }}>
+                        11
                     </div>
                 </div>
             </div>
